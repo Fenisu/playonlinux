98d88c9a01367d5f69b740b78323182c14d86925
Ignacio Quezada <dreamtrick@gmail.com>
2016-01-15 15:41:58 +0100

    Add RA2 and Yuri expansion, for internet found setup.exe

---
+++ Command And Conquer - Red Alert 2 - Yuri's Revenge.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+# Date : (2016-01-15 21-00)
+# Wine version used : 1.9.1
+# Distribution used to test : ArchLinux amd4
+# Author : Fenisu
+# Licence : Retail
+# Only For : http://www.playonlinux.com
+# Script available at: https://github.com/Fenisu/playonlinux
+
+[ "$PLAYONLINUX" = "" ] && exit 0
+source "$PLAYONLINUX/lib/sources"
+
+
+TITLE="Command And Conquer : Red Alert 2 - Yuri's Revenge"
+SHORTCUT_NAME="Command And Conquer : Red Alert 2 - Yuri's Revenge"
+PREFIX="RA2"
+WORKING_WINE_VERSION="1.9.1"
+
+POL_SetupWindow_Init
+POL_SetupWindow_presentation "Command & Conquer: Red Alert 2 - Yuri's Revenge" "Westwood Games" "http://www.westwood.com/" "DemonHypnos" "Red Alert 2 - Yuri's Revenge"
+
+POL_Wine_SelectPrefix "$PREFIX"
+POL_System_SetArch "x86"
+
+cd "$HOME"
+POL_SetupWindow_browse "$(eval_gettext 'Please select the setup file to run.')" "$TITLE"
+SETUP_EXE="$APP_ANSWER"
+POL_Wine start /unix "$SETUP_EXE"
+POL_Wine_WaitExit "$TITLE"
+
+POL_Shortcut "YURI.exe" "$TITLE"
+
+POL_SetupWindow_Close
+exit
