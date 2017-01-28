29496c18e8f5ff20e3942a4faed301a73ace6bbc
Ignacio Quezada <dreamtrick@gmail.com>
2017-01-28 21:40:25 +0100

    AoE2 non-HD edition, autoextract part1.rar

---
+++ Age of Empires 2 rar.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+# Date : (2016-01-15 21-00)
+# Wine version used : 1.9.1
+# Distribution used to test : ArchLinux amd4
+# Author : Fenisu
+# Licence : Retail
+# Only For : http://www.playonlinux.com
+# Script available at: https://github.com/Fenisu/playonlinux
+ 
+[ "$PLAYONLINUX" = "" ] && exit 0
+source "$PLAYONLINUX/lib/sources"
+
+
+TITLE="Age of Empires 2"
+SHORTCUT_NAME="Age of Empires 2"
+PREFIX="AoE2"
+WORKING_WINE_VERSION="1.9.1"
+
+POL_SetupWindow_Init
+POL_SetupWindow_presentation "Age of Empires 2" "Microsoft Games" "http://www.microsoft.com/" "Emblema Studios " "Age of Empires 2 (no-HD)"
+
+POL_Wine_SelectPrefix "$PREFIX"
+POL_System_SetArch "x86"
+POL_Wine_PrefixCreate "$WORKING_WINE_VERSION"
+
+
+POL_SetupWindow_browse "$(eval_gettext 'Please select the part1.rar of the compressed AoE2.')" "$TITLE"
+
+unrar x "$APP_ANSWER" ~/.PlayOnLinux/wineprefix/"$PREFIX"/drive_c
+POL_Wine_WaitExit "$TITLE"
+
+POL_Shortcut "empires2.exe" "$TITLE"
+
+POL_SetupWindow_Close
+exit
